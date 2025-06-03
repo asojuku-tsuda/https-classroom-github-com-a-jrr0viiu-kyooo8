d99497316882686b790d783fff84cb721fe64232
荒殿京介 <147121864+kyooo8@users.noreply.github.com>
2025-06-03 02:17:47 +0000

    課題

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,9 +9,17 @@
     <div class="login-box">
       <h2>
 <?php
+if(isset($_GET['indata'])==false){
+  die("何か入力してください");
+}
 
+$indata = filter_input(INPUT_GET,'indata');
 
-echo "入力された数字は： " . $_GET['indata'];
+if(mb_ereg('^[0-9]+$',$indata)==false){
+  die("番号を入力してください");
+}
+
+echo "入力された数字は： " . $indata;
 ?>
     </h2>
     </div>
--- public/try3/inputuser.php
+++ public/try3/inputuser.php
@@ -9,10 +9,34 @@
     <div class="login-box">
       <h2>
 <?php
+$name = filter_input(INPUT_GET,'username');
+$address = filter_input(INPUT_GET,'useraddress');
+$mail = filter_input(INPUT_GET,'usermail');
+
+if(isset($name)==false){
+  die("名前を入力してください");
+}
+if(isset($address)==false){
+  die("住所を入力してください");
+}
+if(isset($mail)==false){
+  die("メールアドレスを入力してください");
+}
+
+if (mb_strlen($name) > 20 || !preg_match('/^[\p{Hiragana}\p{Katakana}\p{Han}a-zA-Z0-9\s]+$/u', $name)) {
+  die("名前は20文字以内の日本語または英数字で入力してください。");
+}
+if (mb_strlen($address) > 50) {
+  die("住所は50文字以内で入力してください。");
+}
+if (mb_strlen($mail) > 100 || !filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+  die("正しいメールアドレス形式（100文字以内）で入力してください。");
+}
+
 echo "あなたが入力した値<br>";
-echo "名前：" . $_GET['username'] . "<br>";
-echo "住所：" . $_GET['useraddress']. "<br>";
-echo "メールアドレス：" . $_GET['usermail'];
+echo "名前：" . $name . "<br>";
+echo "住所：" . $address . "<br>";
+echo "メールアドレス：" . $mail;
 ?>
     </h2>
     </div>
